1a12214fabf661268865e56260116f08080db9a8
jocnunez <jose.nunnez@gmail.com>
2022-07-08 14:28:01 +0200

    feat: new layouts

--- app/src/main/java/com/jocnunez/notes/MainActivity.kt
+++ app/src/main/java/com/jocnunez/notes/MainActivity.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import com.jocnunez.notes.list.ListActivity
+import com.jocnunez.notes.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, ListActivity::class.java).apply {
-            Log.d("Debug", "Starting List Activity")
+        // TODO: Check configuration and pass parameters to Login Activity if necessary
+        val isLogged = false
+        if (isLogged) {
+            startActivity(Intent(this, ListActivity::class.java).apply { })
+        } else {
+            // TODO: Is firebase or local?
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.putExtra("isLocal", false)
+            startActivity(intent)
         }
-        startActivity(intent)
     }
 }
--- app/src/main/java/com/jocnunez/notes/login/LoginActivity.kt
+++ app/src/main/java/com/jocnunez/notes/login/LoginActivity.kt
@@ -2,11 +2,49 @@ package com.jocnunez.notes.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.FrameLayout
 import com.jocnunez.notes.R
+import com.jocnunez.notes.menu.MenuItems
 
 class LoginActivity : AppCompatActivity() {
+    private var loginContainer: FrameLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginContainer = findViewById(R.id.loginContainer)
+        val isLocal = intent.getBooleanExtra("isLocal", true)
+        if (isLocal) {
+            initLocalJSON()
+        } else {
+            initFirebase()
+        }
+    }
+
+    // Menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        MenuItems().itemHandler(item)
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    // Firebase
+    fun initFirebase() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.loginContainer, LoginFormFragment())
+        transaction.commit()
+    }
+
+    fun initLocalJSON() {
+
     }
 }
