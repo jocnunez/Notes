9982a2a6e3814f50e09de44d457b87e695d335f7
jocnunez <jose.nunnez@gmail.com>
2022-07-08 14:30:32 +0200

    feat: new layouts

---
+++ app/src/main/java/com/jocnunez/notes/login/LoginFormFragment.kt
@@ -0,0 +1,19 @@
+package com.jocnunez.notes.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jocnunez.notes.R
+
+class LoginFormFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login_form, container, false)
+    }
+}
