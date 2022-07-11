a16f7dbb6025b606a50c3715b4f3ba22ef38d946
jocnunez <jose.nunnez@gmail.com>
2022-07-11 09:39:46 +0200

    feat: new layouts

--- app/src/main/java/com/jocnunez/notes/MainActivity.kt
+++ app/src/main/java/com/jocnunez/notes/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
+import com.jocnunez.notes.databinding.ActivityMainBinding
 import com.jocnunez.notes.list.ListActivity
 import com.jocnunez.notes.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding:ActivityMainBinding
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // TODO: Check configuration and pass parameters to Login Activity if necessary
         val isLogged = false
